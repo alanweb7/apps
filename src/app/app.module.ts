606ad147673fa17f4e4d4091e71640e67cc129d5
alanweb7 <alanweb7@gmail.com>
2018-08-14 13:53:00 -0300

    Atualizar KCODE 7

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -23,7 +23,7 @@ import { IonicStorageModule } from '@ionic/storage';
 import { Pro } from '@ionic/pro';
 
 Pro.init('325ddd2e', {
-  appVersion: '19.0.4'
+  appVersion: '19.0.5'
 })
 
 @Injectable()
